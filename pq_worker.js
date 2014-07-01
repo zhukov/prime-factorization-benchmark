b415b24511f2ebe3f0c563529094474201703ca6
Igor Zhukov <igor.beatle@gmail.com>
2014-07-01 20:26:25 +0400

    Added pq worker

---
+++ pq_worker.js
@@ -0,0 +1,17 @@
+/*!
+ * Webogram v0.1.8 - messaging web application for MTProto
+ * https://github.com/zhukov/webogram
+ * Copyright (C) 2014 Igor Zhukov <igor.beatle@gmail.com>
+ * https://github.com/zhukov/webogram/blob/master/LICENSE
+ */
+
+importScripts(
+  'bin_utils.js',
+  'bigint.js',
+  'long.js',
+  'jsbn_combined.js'
+);
+
+onmessage = function (e) {
+  postMessage(pqPrimeFactorization(e.data));
+}
